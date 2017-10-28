6537d6325a65d1e86d647c1f3929f7ff05460173
Viktor <vsh91@users.noreply.github.com>
2017-10-28 19:29:50 +0300

    Fixed typofix in version (#11)

--- minio.rb
+++ minio.rb
@@ -2,7 +2,7 @@ class Minio < Formula
   desc "Amazon S3 compatible object storage server"
   homepage "https://github.com/minio/minio"
   url "https://github.com/minio/minio.git"
-  version "20171027185902Z
+  version "20171027185902Z"
   revision 1
 
   if OS.mac?
