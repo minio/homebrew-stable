5ede9b6c4b7f561b3702f6eca6a2731cf90ab26f
minio-trusted <trusted@minio.io>
2018-03-19 12:25:54 -0700

    fix sha256 for minio release

--- minio.rb
+++ minio.rb
@@ -10,7 +10,7 @@ class Minio < Formula
 
   if OS.mac?
     url "https://dl.minio.io/server/minio/release/darwin-amd64/minio.#{git_tag}"
-    sha256 "cafc2943870fcc2da4131e66b23056aabe0840c17b974fb42f0494578e71c7a9"
+    sha256 "3a3195d57376576feb70a5f0c8255f39a9f9d9e0364a1719e71392a48ed994d9"
   elsif OS.linux?
     raise "No Linux support"
   end
