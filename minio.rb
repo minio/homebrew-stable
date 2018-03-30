c05067e689b6dcd1a9082e21ca25376202f9e8f3
Minio Trusted <trusted@minio.io>
2018-03-30 00:41:28 +0000

    Release version RELEASE.2018-03-30T00-38-44Z

--- minio.rb
+++ minio.rb
@@ -1,6 +1,6 @@
 class Minio < Formula
   # minio specific
-  git_tag = "RELEASE.2018-03-28T23-45-53Z"
+  git_tag = "RELEASE.2018-03-30T00-38-44Z"
 
   desc "Amazon S3 compatible object storage server"
   homepage "https://github.com/minio/minio"
