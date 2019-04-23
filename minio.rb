6c64032d9c77f3a201df5e92964533c5d1c76c4c
Minio Trusted <trusted@minio.io>
2019-04-23 23:54:53 +0000

    Release version RELEASE.2019-04-23T23-50-36Z

--- minio.rb
+++ minio.rb
@@ -1,6 +1,6 @@
 class Minio < Formula
   # minio specific
-  git_tag = "RELEASE.2019-04-18T21-44-59Z"
+  git_tag = "RELEASE.2019-04-23T23-50-36Z"
 
   desc "Amazon S3 compatible object storage server"
   homepage "https://github.com/minio/minio"
@@ -10,7 +10,7 @@ class Minio < Formula
 
   if OS.mac?
     url "https://dl.minio.io/server/minio/release/darwin-amd64/minio.#{git_tag}"
-    sha256 "02eb83ba4571a166117d84e78be6be5b0a914d9deb8666ced2de84af07fa37c7"
+    sha256 "207fd814591e075846b822ef0d410c4ba8deb80361c33d369a691c86be47815f"
   elsif OS.linux?
     raise "No Linux support"
   end
