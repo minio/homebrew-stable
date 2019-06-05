909c1c3f48bf953202a0e944a333f66d92e554c4
Harshavardhana <harsha@minio.io>
2019-06-04 23:01:15 -0700

    Add Linux brew support
    This PR also cleans up macOS installation,
    it removes creating unexpected directories
    on host system. MinIO brew installation
    shouldn't enforce such things. MinIO is meant
    to be run as non-root.

--- mc.rb
+++ mc.rb
@@ -2,7 +2,7 @@ class Mc < Formula
   # mc specific
   git_tag = "RELEASE.2019-05-29T21-21-34Z"
 
-  desc "ls, cp, mkdir, diff and rsync for filesystems and object storage"
+  desc "MinIO Client for object storage and filesystems"
   homepage "https://github.com/minio/mc"
   url "https://github.com/minio/mc.git"
   version git_tag.gsub(%r'[^\d]+', '') + 'Z'
@@ -12,20 +12,17 @@ class Mc < Formula
     url "https://dl.minio.io/client/mc/release/darwin-amd64/mc.#{git_tag}"
     sha256 "30172968ee98171eb5f2745ea508f10c8fab8ad1ea944ca75000b17069dd9699"
   elsif OS.linux?
-    raise "No Linux support"
+    url "https://dl.minio.io/client/mc/release/linux-amd64/mc.#{git_tag}"
+    sha256 "74fac61c9f68a19ae2dedc78bc4a97fc77f319f46d3ab8ecdc966c6116f5c299"
   end
 
   bottle :unneeded
   depends_on :arch => :x86_64
 
-  conflicts_with "midnight-commander", :because => "Both install a `mc` binary"
+  conflicts_with "midnight-commander", :because => "Both install `mc` binary"
 
   def install
     bin.install Dir.glob("mc.*").first => "mc"
-  end
-
-  def post_install
-    (etc/"mc").mkpath
   end
 
   test do
