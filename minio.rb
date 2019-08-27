e8570609ae0bd01bd5d26c7e48a39139e8e099e2
Kaan <kaankabalak@gmail.com>
2019-08-27 16:37:23 -0700

    Add banner with useful links and general info
    This commit also introduces the squiggly heredoc that strips the leading
    whitespace for indentation and readability purposes.
    
    Fixes #30
    Part of #32

--- minio.rb
+++ minio.rb
@@ -23,39 +23,58 @@ class Minio < Formula
     bin.install Dir.glob("minio.*").first => "minio"
   end
 
+  def post_install
+    ohai "Download complete!"
+    ohai "Useful links:"
+    puts <<~EOS
+      Command-line Access: https://docs.min.io/docs/minio-client-quickstart-guide
+
+      Object API (Amazon S3 compatible):
+         Go:         https://docs.min.io/docs/golang-client-quickstart-guide
+         Java:       https://docs.min.io/docs/java-client-quickstart-guide
+         Python:     https://docs.min.io/docs/python-client-quickstart-guide
+         JavaScript: https://docs.min.io/docs/javascript-client-quickstart-guide
+         .NET:       https://docs.min.io/docs/dotnet-client-quickstart-guide
+
+      Talk to the community: https://slack.min.io
+    EOS
+    ohai "Get started:"
+    puts `#{bin}/minio server -h`
+  end
+
   test do
-    (testpath/"config.json").write <<-EOS
-{
-        "version": "14",
-        "credential": {
-                "accessKey": "minio",
-                "secretKey": "minio123"
-        },
-        "region": "us-east-1",
-        "browser": "on",
-        "logger": {
-                "console": {
-                        "level": "error",
-                        "enable": true
-                },
-                "file": {
-                        "level": "error",
-                        "enable": false,
-                        "filename": ""
-                }
-        },
-        "notify": {
-                "redis": {
-                        "1": {
-                                "enable": true,
-                                "address": "127.0.0.1:6379",
-                                "password": "",
-                                "key": "minio_events"
-                        }
-                }
-        }
-}
-EOS
+    (testpath/"config.json").write <<~EOS
+      {
+              "version": "14",
+              "credential": {
+                      "accessKey": "minio",
+                      "secretKey": "minio123"
+              },
+              "region": "us-east-1",
+              "browser": "on",
+              "logger": {
+                      "console": {
+                              "level": "error",
+                              "enable": true
+                      },
+                      "file": {
+                              "level": "error",
+                              "enable": false,
+                              "filename": ""
+                      }
+              },
+              "notify": {
+                      "redis": {
+                              "1": {
+                                      "enable": true,
+                                      "address": "127.0.0.1:6379",
+                                      "password": "",
+                                      "key": "minio_events"
+                              }
+                      }
+              }
+      }
+    EOS
     minio_io = IO.popen("#{bin}/minio --config-dir #{testpath} server #{testpath}/export", :err=>[:child, :out])
     sleep 1
     Process.kill("INT", minio_io.pid)
