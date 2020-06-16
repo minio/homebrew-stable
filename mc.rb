c58d7cafaaa2451aa4b46d923ec0f3be85a34eb4
Minio Trusted <trusted@minio.io>
2020-06-16 19:28:29 +0000

    Release version RELEASE.2020-06-16T19-24-41Z

--- mc.rb
+++ mc.rb
@@ -1,6 +1,6 @@
 class Mc < Formula
   # mc specific
-  git_tag = "RELEASE.2020-05-28T23-43-36Z"
+  git_tag = "RELEASE.2020-06-16T19-24-41Z"
 
   desc "MinIO Client for object storage and filesystems"
   homepage "https://min.io"
@@ -10,7 +10,7 @@ class Mc < Formula
 
   if OS.mac?
     url "https://dl.minio.io/client/mc/release/darwin-amd64/mc.#{version}"
-    sha256 "6d0fdc304cf18740a7c2fea395cb50c52b3bba5408d3acc301cd924a08f9554c"
+    sha256 "7c8d5f9c7711d6d18d9a58d0a1fe4be752ec4f8d2d1ea98be6c258bf4d6cf2f3"
   elsif OS.linux?
     url "https://dl.minio.io/client/mc/release/linux-amd64/mc.#{version}"
     sha256 "5de3fc5bd45f119437d877abecc40bd1dc7e584d1bf7fd7f12f6b3ea4febedb9"
