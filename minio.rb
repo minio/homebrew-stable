28dd7d3a58a0129152377230557212eb086ecdf2
Minio Trusted <trusted@minio.io>
2020-10-09 23:13:49 +0000

    Release version RELEASE.2020-10-09T22-55-05Z

--- minio.rb
+++ minio.rb
@@ -1,6 +1,6 @@
 class Minio < Formula
   # minio specific
-  git_tag = "RELEASE.2020-10-03T02-19-42Z"
+  git_tag = "RELEASE.2020-10-09T22-55-05Z"
 
   desc "High Performance Object Storage compatible with Amazon S3 API"
   homepage "https://min.io"
@@ -10,7 +10,7 @@ class Minio < Formula
 
   if OS.mac?
     url "https://dl.minio.io/server/minio/release/darwin-amd64/minio.#{version}"
-    sha256 "87985be665502701d7c28f08e1d96f7a3156e06d3a8c634935f0bf7b49bab6e9"
+    sha256 "c3e5cd6c6d46dd61b5f441e0e387a620c8cec723748cbd7979d0995689f46566"
   elsif OS.linux?
     url "https://dl.minio.io/server/minio/release/linux-amd64/minio.#{version}"
     sha256 "f8ed8763bef91fc72c6fc9747406e482cbbf561747765d3b8b3cb0f31303a7c6"
