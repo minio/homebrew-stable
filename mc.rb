0dc886a2386f0861054610abacb8ca09f3db7b9d
Minio Trusted <trusted@minio.io>
2021-02-10 07:42:55 +0000

    Release version RELEASE.2021-02-10T07-32-57Z

--- mc.rb
+++ mc.rb
@@ -1,6 +1,6 @@
 class Mc < Formula
   # mc specific
-  git_tag = "RELEASE.2021-02-07T02-02-05Z"
+  git_tag = "RELEASE.2021-02-10T07-32-57Z"
 
   desc "MinIO Client for object storage and filesystems"
   homepage "https://min.io"
@@ -10,10 +10,10 @@ class Mc < Formula
 
   if OS.mac?
     url "https://dl.minio.io/client/mc/release/darwin-amd64/mc.#{version}"
-    sha256 "1f7934e453209ff0706404c10c67e8e9e0ad8db82effb4e63a708da926d7fb12"
+    sha256 "9a6479977f646de226f10c300484bb9a029f6a4aca47d8755f6b2d86ea94d329"
   elsif OS.linux?
     url "https://dl.minio.io/client/mc/release/linux-amd64/mc.#{version}"
-    sha256 "b1cbf677736c434caa779328aa9696530692af11ad8864fe8171d5c3a7d865a4"
+    sha256 "3657ab40ec55948e4165346e3f1dbb20671bc0f843827d1d8fad309ac446ed22"
   end
 
   bottle :unneeded
