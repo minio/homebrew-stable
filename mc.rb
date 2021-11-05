0e8e8397e332c8bb4416f3605cdea98e17b10227
Minio Trusted <trusted@minio.io>
2021-11-05 10:20:39 +0000

    Release version RELEASE.2021-11-05T10-05-06Z

--- mc.rb
+++ mc.rb
@@ -1,6 +1,6 @@
 class Mc < Formula
   # mc specific
-  git_tag = "RELEASE.2021-10-07T04-19-58Z"
+  git_tag = "RELEASE.2021-11-05T10-05-06Z"
 
   desc "MinIO Client for object storage and filesystems"
   homepage "https://min.io"
@@ -14,11 +14,11 @@ class Mc < Formula
       sha256 "3d25ab5da5959fcf2339203881faeaae2fe606f2e55a716b20e13a7628673589"
     else
       url "https://dl.minio.io/client/mc/release/darwin-amd64/archive/mc.#{version}"
-      sha256 "100c5b1185e8a28d44fa00d1532b6c2ef44e0c57f476e36d7e7dbfc46ed1f658"
+      sha256 "e97fde6e02954b7bad4187dce79d9908e2f18e0ceb92bb17ca165cb71aa60f08"
     end
   elsif OS.linux?
     url "https://dl.minio.io/client/mc/release/linux-amd64/archive/mc.#{version}"
-    sha256 "aa58e16c74c38bc05ecf73bedee476eafb3a1c42ea1ac95635853b530a36be93"
+    sha256 "9f5ad72fd93678f9f756c6da34a8d4d1e61f7356a96ee327c652ad765975d2b2"
   end
 
   conflicts_with "midnight-commander", :because => "Both install `mc`"
